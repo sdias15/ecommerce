6d2cbe167964b8c9d507fa6bbc6fcd5bd5e1c387
Sílvio Dias <esisildi@gmail.com>
2018-07-20 14:08:53 +0100

    tpl site

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 require_once("vendor/autoload.php");
 
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
@@ -13,6 +18,16 @@ $app->get('/', function() {
 	$page->setTpl("index");
 
 });
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+
+});
+
+
 
 $app->run();
 
