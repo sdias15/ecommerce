6c50ba7674c66aa2cba598e6d8e1da8ea786a472
Sílvio Dias <esisildi@gmail.com>
2018-07-23 22:17:59 +0100

    Parte Login

--- index.php
+++ index.php
@@ -1,47 +1,47 @@
 <?php 
-
 require_once("vendor/autoload.php");
-
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
-
+use \Hcode\Model\user;
 
 $app = new \Slim\Slim();
-
 $app->config('debug', true);
-
 $app->get('/', function() {
     
 	$page = new Hcode\Page();
-
 	$page->setTpl("index");
-
 });
+
 
 $app->get('/admin', function() {
     
 	$page = new PageAdmin();
-
 	$page->setTpl("index");
-
 });
 
-$app->get('/login', function() {
+
+
+$app->get('/admin/login', function() {
     
 	$page = new PageAdmin([
 		"header"=>false,
 		"footer"=>false
-
 	]);
-
 	$page->setTpl("login");
 	
+});
+
+$app->post('/admin/login', function(){
+
+User::login($_POST["login"], $_POST["password"]);
+
+header("Location: /admin");
+exit;
+
 
 });
 
 
-
 $app->run();
-
  ?>
--- views-cache/login.d70a64ba7abb00fe427240570e3a4447.rtpl.php
+++ views-cache/login.d70a64ba7abb00fe427240570e3a4447.rtpl.php
@@ -27,19 +27,19 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="/res/admin/index2.html"><b>Admin</b>LTE</a>
+    <a href="/res/admin/index2.html"><b>Admin</b>LTE SDias</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="res/admin/index2.html" method="post">
+    <form action="/admin/login" method="post">
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        <input type="text" class="form-control" placeholder="Login" name="login">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
+        <input type="password" class="form-control" placeholder="Password" name="password">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
